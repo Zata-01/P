22908dd35192aad1227580d464043fabc5a8708f
Ramón Eduardo Valdez Arámburo <jesusramonvaldez1@gmail.com>
2025-12-04 18:58:42 -0700

    feat: Se terminó el controlador Productos, así mismo se testeó correctamente

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -1,13 +1,36 @@
 export const getProductos = (req, res) => {
-  res.json([
-    { id: 1, nombre: "Rolex Daytona", precio: 100 },
-    { id: 2, nombre: "Cartier Crash", precio: 200 },
-    { id: 3, nombre: "Seiko Ultimate", precio: 300 },
-    { id: 4, nombre: "Bulova PV", precio: 400 },
-    ]);
+  res.json(productos)//regresa todos los productos, incluyendo los nuevos que se agregan
 };
-/*
-export const addProductos
-export const updateProductos
-export const deleteProductos
-*/ //METODOS PENDIENTES DE HACER^
+
+let productos = [ //BDD temporal
+  {id:1, nombre:"Rolex Daytona", precio:100},
+  { id: 2, nombre: "Cartier Crash", precio: 200 },
+  { id: 3, nombre: "Seiko Ultimate", precio: 300 },
+  { id: 4, nombre: "Bulova PV", precio: 400}
+
+];
+
+export const addProducts = (req,res) =>{
+  const nuevo = { //creación del nuevo producto con lo que le envíe el cliente
+    id:productos.length + 1,
+    nombre: req.body.nombre,
+    precio: req.body.precio
+  }
+  productos.push(nuevo)//se guarda el producto en el arreglo
+  res.json(nuevo)//envia al cliente el producto que agrego, en este caso será el admin
+}
+export const updateProducts = (req, res) =>{
+  const id = parseInt(req.params.id)//toma el id de la url
+  const index = productos.findIndex(p => p.id ===id); //busca al posicióon del indice del producto con ese id
+  if(index === -1) return res.status(404).json({message:"Producto no encontrado"});
+  productos[index] = {...productos[index],...req.body};//actualiza el producto mezclando lo que ya tenia con lo  nuevo del cliente
+  res.json(productos[index]) //devuelve el producto actualizado
+}
+
+export const deleteProducts = (req, res) =>{
+  const id = parseInt(req.params.id)
+  const index = productos.findIndex(p => p.id ===id );
+  if(index=== -1)return res.status(404).json({message:"Producti no encontrado"})
+    productos.splice(index, 1);
+  res.json({message: "Producto eliminado correctamente"})
+}
--- routes/productos.routes.js
+++ routes/productos.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import {getProductos, addProductos, updateProductos, deleteProductos} from "../controllers/productos.controller.js";
+import {getProductos, addProducts, updateProducts, deleteProducts} from "../controllers/productos.controller.js";
 
 const router = Router();
 router.get("/productos", getProductos);
+router.post("/productos", addProducts );
+router.put("/productos/:id", updateProducts) //Actualiza productos por ID
+router.delete("/productos/:id", deleteProducts ) //Elimina productos por ID
+
 
 export default router;
