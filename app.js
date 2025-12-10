f5f98f7cf2a263dcd30c0e55860b4051ac9555ac
ernesto ramos <ernestorms@protonmail.com>
2025-12-09 17:54:16 -0700

    Implementación de JWTs y creación de sesiones de usuario

--- app.js
+++ app.js
@@ -1,20 +1,39 @@
 import express from 'express'
-import { PORT } from './config/config.js'
+import { PORT, SECRET_JWT_KEY } from './config/config.js'
 import { UserRepository } from './user-repository.js'
+import cookieParser from 'cookie-parser'
+import jwt from 'jsonwebtoken'
 
 const app = express()
 app.set('view engine', 'ejs')
 app.use(express.json())
+app.use(cookieParser())
 
 app.get('/', (req, res) => {
-  res.render('index')
+  const token = req.cookies.access_token
+  if (!token) return res.render('index')
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    res.render('index', data)
+  } catch (error) {
+    res.render('index')
+  }
 })
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body
   try {
     const user = await UserRepository.login({ username, password })
-    res.send({ user })
+    const token = jwt.sign({ id: user._id, username: user.username }, SECRET_JWT_KEY, {
+      expiresIn: '1h'
+    })
+    res.cookie('access_token', token, {
+      httpOnly: true, // acessible solamente del lado del servidor
+      secure: process.env.NODE_ENV === 'production', // cookie accesible solamente en https
+      sameSite: 'strict', // cookie accesible solamente desde el mismo dominio
+      maxAge: 1000 * 60 * 60 // 1 hora máximo de validez para la cookie
+    }).send({ user })
   } catch (error) {
     res.status(401).send(error.message)
   }
@@ -34,7 +53,17 @@ app.post('/register', async (req, res) => {
 app.post('/logout', (req, res) => {})
 
 app.get('/protected', (req, res) => {
-  res.render('protected')
+  const token = req.cookies.access_token
+  if (!token) {
+    return res.status(403).send('Access not authorized')
+  }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    res.render('protected', data)
+  } catch (error) {
+    res.status(401).send('Access not authorized')
+  }
 })
 
 app.listen(PORT, () => {
--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 export const {
   PORT = 3000,
-  SALT_ROUNDS = 10
+  SALT_ROUNDS = 10,
+  SECRET_JWT_KEY = 'VN3hZz14PQrOAfRHGk25LKje3IR3Kch4rl1EK1rK'
 } = process.env
