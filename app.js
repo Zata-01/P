c47051389347eb5218e7ef21babade61239d5765
ernesto ramos <ernestorms@protonmail.com>
2025-12-09 18:07:50 -0700

    Funcionalidad de logout terminada

--- app.js
+++ app.js
@@ -53,7 +53,9 @@ app.post('/register', async (req, res) => {
     res.status(400).send(error.message)
   }
 })
-app.post('/logout', (req, res) => {})
+app.post('/logout', (req, res) => {
+  res.clearCookie('access_token').json({ message: 'Logout successful' })
+})
 
 app.get('/protected', (req, res) => {
   const { user } = req.session
